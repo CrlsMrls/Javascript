4491830fabf07e2f33baa901e24f18a6d634b1c9
Carlos Morales <morales.carlos@outlook.com>
2013-08-04 13:38:56 +0200

    name of function can be reference only from inside

--- Functions/references.js
+++ Functions/references.js
@@ -9,23 +9,37 @@ Functions are objects, therefore referenced. So they can:
  - Plus they can be invoked
 */
 
+// assign anonymous function to variable
+var addNumbers = function (a, b) {
+	return a + b;
+};
 
+// assign named function expression to variable
+var subtractNumbers = function subtract (a, b) {
+	return a - b;
+}
+
+// the name of a named function expression can be referenced only from inside its body
+console.log( 'typeof subtract: ' + typeof subtract ); // => typeof subtract: undefined
+
+// object stores anonymous function as a method
 var myObject = {
-
-	// function as a method
-	sayHola: function (str) {
-		console.log('Hola! ' + str );
+	dummyProperty: 0 ,
+	sayHi: function (str) {
+		console.log('Hi! ' + str );
 	}
 };
 
-// invoked
-myObject.sayHola('method'); // => Hola! method
+// method invocation
+myObject.sayHi('from method'); // => Hi! from method
 
-// store in arrays
-var myArray = [myObject.sayHola];
-myArray[0]('array'); // => Hola! array
+// variable pointing to an anonymous method
+var reference = myObject.sayHi;
+reference('from reference'); // => Hi! from reference
 
-// reference
-var reference = myObject.sayHola;
-reference('reference'); // => Hola! reference
+// store functions in arrays
+var myArray = [myObject.sayHi, addNumbers, subtractNumbers];
+myArray[0]('from array'); // => Hi! from array
+console.log(myArray[1](3,2)); // => 5
+console.log(myArray[2](3,2)); // => 1
 
