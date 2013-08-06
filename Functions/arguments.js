d45e575737831d4267944496ecb8f76bd222a108
carlos.morales <carlos.morales@mydepotcheck.com>
2013-08-06 14:36:59 +0200

    function.length and arguments.length, simplification of sum function

--- Functions/arguments.js
+++ Functions/arguments.js
@@ -1,24 +1,26 @@
 // If there is a different number of arguments than there are parameters, no error is raised
 
-var sum = function(a,b) {
+var sum = function sumName(a,b) {
+
+	console.log("expecting "+ sumName.length + " arguments, but received " + arguments.length);
 	return a + b;
 }
 
-console.log( sum(1,2,3,4,5,6) ); // => 3
+// no error is given
+var value = sum(1,2,3,4,5,6,7,8,9); // => expecting 2 arguments, but received 6
+
+console.log( value ); // => 3
 
 
-// The arguments parameter is a collection of all of the arguments passed to the function
-var sum = function(){
+// The 'arguments' variable is an array of all of the arguments passed to the function
+sum = function(){
 	var result = 0;
-	console.log("arguments.length: " + arguments.length);
-	
 	for (var i = arguments.length - 1; i >= 0; i--) {
 		result += arguments[i];
 	};
 	return result;
 }
 
-console.log( sum(1,2,3) ); // => arguments.length: 3 // => 6
-
-console.log( sum(1,2,3,4,5,6,7,8,9) ); // => arguments.length: 9 // => 45
+console.log( sum(1,2,3) ); // => 6
+console.log( sum(1,2,3,4,5,6,7,8,9) ); // => 45
 
