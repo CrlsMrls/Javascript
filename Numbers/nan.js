a834388e74ec0e426efdb84ee1b7caf6450f2d63
carlos.morales <morales.carlos@outlook.com>
2013-08-12 00:10:43 +0200

    new Number.js and improved NaN.js with isNumber function

--- Numbers/nan.js
+++ Numbers/nan.js
@@ -10,11 +10,13 @@ console.log(wrongNumber); // => NaN
 console.log(typeof wrongNumber); // => number
 
 // never compare to NaN directly
-if (wrongNumber !== NaN) console.log('A NaN differs from another NaN');
-if (wrongNumber !== wrongNumber) console.log('Actually a NaN differs from itself!');
+if (wrongNumber === NaN) console.log('not executing');
+else console.log('A NaN differs from another NaN'); // => A NaN differs ...
+if (wrongNumber === wrongNumber) console.log('not executing');
+else console.log('Actually a NaN differs from itself!'); // => Actually a ...
 
 // compare with isNaN() function
-if (isNaN(wrongNumber)) console.log('isNaN works as expected');
+if (isNaN(wrongNumber)) console.log('isNaN works as expected'); // => isNaN works ...
 
 // it also works with strings which are not numbers
 if (isNaN('text')) console.log('"text" isNaN');
@@ -23,7 +25,15 @@ console.log(infinite); // => Infinity
 
 // careful, Infinity is a number type but not NaN
 if (isNaN(infinite)) console.log('not executing');
-else console.log('infinite is not a NaN');
+else console.log('infinite is not a NaN'); // => infinite ...
 
+// check if number is valid and finite
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
+console.log(isNumber(wrongNumber)); // => false
+console.log(isNumber(infinite)); // => false
+console.log(isNumber('-1.5')); // => true
+console.log(isNumber(.235)); // => true
 
