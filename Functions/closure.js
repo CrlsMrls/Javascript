9ffea8fbf77dd427863c1e54e3d52163f6ed8ac6
carlos.morales <morales.carlos@outlook.com>
2014-05-19 11:17:27 +0200

    improve naming of closure.js variables

--- Functions/closure.js
+++ Functions/closure.js
@@ -1,24 +1,24 @@
 // A closure allows a function to access those non-local variables even when invoked outside scope
 // if you use the function keyword inside another function, you are creating a closure.
 
-var fun;
+var funct;
 
 var container = function() {
 	var innerScopeVar = 123;
 
 	// copying a reference of the function, it also gives access to the actual scope (closure)
-	fun = function() { console.log('innerScopeVar: ' + innerScopeVar); };
+	funct = function() { console.log('innerScopeVar: ' + innerScopeVar); };
 };
 
-console.log('fun: ' + typeof fun); // => fun: undefined
+console.log('funct: ' + typeof funct); // => funct: undefined
 
-// invoking the outer function, the inner function is assigned to 'fun'
+// invoking the outer function, the inner function is assigned to 'funct'
 container(); 
-console.log('fun: ' + typeof fun); // => fun: function
+console.log('funct: ' + typeof funct); // => funct: function
 
-// 'innerScopeVar' has a longer lifetime than 'container' because 'fun' keeps the closure
+// 'innerScopeVar' has a longer lifetime than 'container' because 'funct' keeps the closure
 container = null;
-fun(); // => innerScopeVar: 123
+funct(); // => innerScopeVar: 123
 
 
 
@@ -32,21 +32,12 @@ var attachClickEvent = function(element, text){
 };
 
 // good
-<<<<<<< HEAD
-var show = function show(a){
-	return function() { console.log('cliked! ' + a); };
-}
-var attachClickEvent = function(element, a){
-	element.onclick = show(a);
-}
-=======
 var show = function show(text){
 	return function() { console.log('cliked! ' + text); };
 };
 var attachClickEvent = function(element, text){
 	element.onclick = show(text);
 };
->>>>>>> ee339f698aa76c7cde9cfc575bfe60905d34e0dc
 
 var button = {};
 attachClickEvent(button, 'message');
